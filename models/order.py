24329fa354a3245b492778890e1650618a7cc875
Naefunn <nathan.d.foster1@gmail.com>
2022-07-07 09:13:25 +0100

    first commit

---
+++ models/order.py
@@ -0,0 +1,13 @@
+
+
+
+class Order:
+
+    def __init__(self, cust_id, customer_name, order_date, product_name, quantity, link):
+        self.customer_name = customer_name
+        self.cust_id = cust_id
+        self.order_date = order_date
+        self.product_name = product_name
+        self.quantity = quantity
+        self.link = link
+        
